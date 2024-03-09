23eb8f9cc25831433e52a7de6a46f932004dd317
zhaoyudong <806094224@qq.com>
2024-03-09 18:54:54 +0800

    记录相关联表查询sql

--- src/main/java/com/wms/entity/RecordRes.java
+++ src/main/java/com/wms/entity/RecordRes.java
@@ -7,4 +7,7 @@ public class RecordRes extends Record {
 
     private String username;
     private String adminname;
+    private String goodsname;
+    private String storagename;
+    private String goodstypename;
 }
